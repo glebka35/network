036167e37934739bef08e3a4c5f22b3af2711d4f
Gleb Uvarkin <uvarkin@vtb.ru>
2021-06-22 23:13:13 +0300

    Some improvements

--- Traffic_Control(task2)/client/run.sh
+++ Traffic_Control(task2)/client/run.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+server=172.72.1.0      # server IP
+port=22                 # port
+connect_timeout=5       # Connection timeout
+
 iperf -seu -p 8080 &
 iperf -se -p 8080 &
 
 while true; do
-    sshpass -p "test" ssh -oStrictHostKeyChecking=no -oConnectTimeout=8 \
-    -i id_rsa test@172.72.1.0 echo "SSH request was succeeded!"
+    #sshpass -p "test" ssh -oStrictHostKeyChecking=no -oConnectTimeout=8 -i id_rsa test@172.72.1.0 && echo "SSH request was succeeded!"
+    
+
+    timeout $connect_timeout bash -c "</dev/tcp/$server/$port"
+    if [ $? == 0 ];then
+        echo "SSH Connection to $server over port $port is possible"
+    else
+        echo "SSH connection to $server over port $port is not possible"
+    fi
+
     ping 172.72.1.0 -c 5
 done
--- Traffic_Control(task2)/server/run.sh
+++ Traffic_Control(task2)/server/run.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-ip link add ifb0 type ifb
-ip link set ifb0 up
-ip a
-
-tc qdisc add dev eth0 ingress
-tc filter add dev eth0 parent ffff: protocol ip u32 match u32 0 0 action mirred egress redirect dev ifb0
-tc qdisc add dev ifb0 root handle 1:0 htb default 1
+tc qdisc del dev eth0 root
+tc qdisc add dev eth0 root handle 1:0 htb default 14
 
 # Задаем пропускную способность
-tc class add dev ifb0 parent 1:0 classid 1:1 htb rate 100mbit prio 1
+tc class add dev eth0 parent 1:0 classid 1:1 htb rate 100mbit 
 
 # Дропаем ICMP
-tc filter add dev ifb0 protocol ip parent 1:0 prio 1 u32 match ip protocol 1 0xff action drop
+tc filter add dev eth0 protocol ip parent 1:0 prio 1 u32 match ip protocol 1 0xff action drop
 
 # Дропаем ssh на 3 клиенте
-tc filter add dev ifb0 protocol ip parent 1:0 prio 1 u32 match ip src 172.20.3.0/24 match ip protocol 6 0xff match ip dport 22 0xffff action drop
+tc filter add dev eth0 protocol ip parent 1:0 prio 0 u32 match ip src 172.72.3.0/24 match ip protocol 6 0xff match ip sport 22 0xffff action drop
 
-tc qdisc add dev eth0 root handle 1:0 htb default 14
 tc class add dev eth0 parent 1:0 classid 1:10 htb rate 100mbit prio 1
 tc class add dev eth0 parent 1:10 classid 1:11 htb rate 5mbit ceil 40mbit prio 1
 tc class add dev eth0 parent 1:10 classid 1:12 htb rate 5mbit ceil 20mbit prio 1
@@ -35,12 +29,12 @@ service ssh status
 
 while true; do
     sleep 7
-    iperf -c 172.72.1.1 -p 8080 -e -i 1 -t 1 &
-    iperf -c 172.72.1.1 -u -p 8080 -e -i 1 -t 1 &
-    iperf -c 172.72.2.2 -p 8080 -e -i 1 -t 1 &
-    iperf -c 172.72.2.2 -u -p 8080 -e -i 1 -t 1 &
-    iperf -c 172.72.3.3 -p 8080 -e -i 1 -t 1 &
-    iperf -c 172.72.3.3 -u -p 8080 -e -i 1 -t 1 &
-    iperf -c 172.72.4.4 -p 8080 -e -i 1 -t 1 &
-    iperf -c 172.72.4.4 -u -p 8080 -e -i 1 -t 1 &
+    #iperf -c 172.72.1.1 -p 8080 -e -i 1 -t 1 &
+    #iperf -c 172.72.1.1 -u -p 8080 -e -i 1 -t 1 &
+    #iperf -c 172.72.2.2 -p 8080 -e -i 1 -t 1 &
+    #iperf -c 172.72.2.2 -u -p 8080 -e -i 1 -t 1 &
+    #iperf -c 172.72.3.3 -p 8080 -e -i 1 -t 1 &
+    #iperf -c 172.72.3.3 -u -p 8080 -e -i 1 -t 1 &
+    #iperf -c 172.72.4.4 -p 8080 -e -i 1 -t 1 &
+    #iperf -c 172.72.4.4 -u -p 8080 -e -i 1 -t 1 &
 done
